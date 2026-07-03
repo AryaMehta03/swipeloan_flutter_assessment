4293fa9986f5ebcb3c62efffd4625a7aa068b8fe
AryaMehta03 <aryaamitmehta@gmail.com>
2026-07-04 00:13:55 +0530

    assets: configured paths

--- lib/core/constants/app_assets.dart
+++ lib/core/constants/app_assets.dart
@@ -19,4 +19,17 @@ class AppAssets {
   static const String billDth = 'assets/images/PYB03.png';
   static const String billMobilePostpaid = 'assets/images/PYB04.png';
   static const String billBroadband = 'assets/images/PYB05.png';
+  static const String debitWallet = 'assets/images/debit_wallet.png';
+  static const String creditWallet = 'assets/images/credit_wallet.png';
+  static const String exploreScorePredictor = 'assets/images/explore_meter.png';
+  static const String exploreCalculator ='assets/images/explore_calculator.png';
+  static const String exploreMoneyBag = 'assets/images/explorer_moneybag.png';
+  static const String helpBannerWoman = 'assets/images/help_banner_person.png';
+  static const String headsetIcon = 'assets/images/help_headset.png';
+  static const String navCreditCard = 'assets/images/bottom_nav_credit_card.png';
+  static const String navInstantLoan = 'assets/images/bottom_nav_rupee.png';
+  static const String navHome = 'assets/images/bottom_nav_home.png';
+  static const String navCip = 'assets/images/bottom_nav_rectangle.png';
+  static const String navOtherLoan = 'assets/images/bottom_nav_rupee02.png';
+  
 }
