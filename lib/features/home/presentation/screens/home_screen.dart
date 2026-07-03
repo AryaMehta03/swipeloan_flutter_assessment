94275d7e43187609a04c90041c1a48a534a5a781
AryaMehta03 <aryaamitmehta@gmail.com>
2026-07-04 00:14:24 +0530

    feat: implement custom floating bottom navigation

--- lib/features/home/presentation/screens/home_screen.dart
+++ lib/features/home/presentation/screens/home_screen.dart
@@ -1,8 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:swipeloan_flutter_assessment/features/home/presentation/widgets/credit_card_payment_card.dart';
+import 'package:swipeloan_flutter_assessment/features/home/presentation/widgets/custom_bottom_nav.dart';
+import 'package:swipeloan_flutter_assessment/features/home/presentation/widgets/explore_more_section.dart';
+import 'package:swipeloan_flutter_assessment/features/home/presentation/widgets/help_banner_card.dart';
 import 'package:swipeloan_flutter_assessment/features/home/presentation/widgets/interest_rate_card.dart';
 import 'package:swipeloan_flutter_assessment/features/home/presentation/widgets/loan_option_card.dart';
 import 'package:swipeloan_flutter_assessment/features/home/presentation/widgets/pay_bills_section.dart';
+import 'package:swipeloan_flutter_assessment/features/home/presentation/widgets/track_spends_card.dart';
 
 import '../../../../core/constants/app_colors.dart';
 import '../widgets/header_section.dart';
@@ -36,12 +40,19 @@ class HomeScreen extends StatelessWidget {
                   const LoanOptionCard(),
                   const SizedBox(height: 13),
                   const PayBillsSection(),
+                  const SizedBox(height: 13),
+                  const TrackSpendsCard(),
+                  const SizedBox(height: 21),
+                  const ExploreMoreSection(),
+                  const SizedBox(height: 18),
+                  const HelpBannerCard(),
                 ],
               ),
             ),
           ],
         ),
       ),
+      bottomNavigationBar: const CustomBottomNav(),
     );
   }
 }
---
+++ lib/features/home/presentation/widgets/custom_bottom_nav.dart
@@ -0,0 +1,147 @@
+import 'dart:math' as math;
+
+import 'package:flutter/material.dart';
+
+import '../../../../core/constants/app_assets.dart';
+import '../../../../core/constants/app_colors.dart';
+
+class CustomBottomNav extends StatelessWidget {
+  const CustomBottomNav({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      top: false,
+      child: SizedBox(
+        height: 88,
+        child: Stack(
+          clipBehavior: Clip.none,
+          alignment: Alignment.topCenter,
+          children: [
+            Positioned.fill(
+              top: 14,
+              child: Container(
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(100),
+                  boxShadow: [
+                    BoxShadow(
+                      color: AppColors.primary.withOpacity(.12),
+                      blurRadius: 18,
+                      spreadRadius: 2,
+                    ),
+                  ],
+                ),
+                child: const Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 18),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      _NavItem(
+                        icon: AppAssets.navCreditCard,
+                        label: 'Credit Card',
+                      ),
+                      _NavItem(
+                        icon: AppAssets.navInstantLoan,
+                        label: 'Instant Loan',
+                      ),
+                      SizedBox(width: 66),
+                      _NavItem(
+                        icon: AppAssets.navCip,
+                        label: 'CIP',
+                      ),
+                      _NavItem(
+                        icon: AppAssets.navOtherLoan,
+                        label: 'Other Loan',
+                        selected: true,
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+
+            const _HomeButton(),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class _HomeButton extends StatelessWidget {
+  const _HomeButton();
+
+  @override
+  Widget build(BuildContext context) {
+    return Transform.rotate(
+      angle: -math.pi / 4,
+      child: Container(
+        width: 63,
+        height: 63,
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(10),
+          gradient: const LinearGradient(
+            colors: [
+              Color(0xff32A763),
+              Color(0xff157E77),
+            ],
+          ),
+        ),
+        child: Transform.rotate(
+          angle: math.pi / 4,
+          child: Center(
+            child: Image.asset(
+              AppAssets.navHome,
+              width: 19,
+              height: 21,
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class _NavItem extends StatelessWidget {
+  const _NavItem({
+    required this.icon,
+    required this.label,
+    this.selected = false,
+  });
+
+  final String icon;
+  final String label;
+  final bool selected;
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      width: 60,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Image.asset(
+            icon,
+            width: 16,
+            height: 16,
+            fit: BoxFit.contain,
+          ),
+          const SizedBox(height: 7),
+          Text(
+            label,
+            textAlign: TextAlign.center,
+            style: TextStyle(
+              fontFamily: 'Inter',
+              fontSize: 7,
+              fontWeight: FontWeight.w500,
+              color: selected
+                  ? AppColors.primary
+                  : Colors.black,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+} 
